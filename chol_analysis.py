fc42982135587fc3c03a0e3fe00131f19869f8c2
Michael Wong <michael.k.wong@duke.edu>
2020-01-24 12:54:20 -0500

    Added LDL section

--- chol_analysis.py
+++ chol_analysis.py
@@ -5,7 +5,19 @@ def HDL_analysis(HDL_level):
         return "Borderline low"
     else:
         return "Low"
-
+      
+        
+def LDL_analysis(LDL_level):
+    if LDL_level < 130:
+        return "Normal"
+    elif 130 <= LDL_level <= 159:
+        return "Borderline high"
+    elif 160 <= LDL_level <= 189:
+        return "High"
+    elif 190 <= LDL_level:
+        return "Very high"
+    else:
+        return "You're dead"
 
 def cholesterol_analysis():
     print("Cholesterol Analysis")
@@ -13,6 +25,9 @@ def cholesterol_analysis():
     test_info = HDLinput.split("=")
     if test_info[0] == "HDL":
         answer = HDL_analysis(int(test_info[1]))      
+        print("The level is {}".format(answer))
+    if test_info[0] == "LDL":
+        answer = LDL_analysis(int(test_info[1]))
         print("The level is {}".format(answer))
 
 
