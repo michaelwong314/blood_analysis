6b93284f08ff8d9dc38d10c2dbbc1f0c96863de6
Michael Wong <michael.k.wong@duke.edu>
2020-01-31 12:36:03 -0500

    Added LDL and HD tests

--- chol_analysis.py
+++ chol_analysis.py
@@ -6,7 +6,7 @@ def HDL_analysis(HDL_level):
     else:
         return "Low"
 
-        
+
 def LDL_analysis(LDL_level):
     if LDL_level < 130:
         return "Normal"
@@ -19,29 +19,32 @@ def LDL_analysis(LDL_level):
     else:
         return "You're dead"
 
+
 def cholesterol_analysis():
     print("Cholesterol Analysis")
     HDLinput = input("Enter test result: ")
     test_info = HDLinput.split("=")
     if test_info[0] == "HDL":
-        answer = HDL_analysis(int(test_info[1]))      
+        answer = HDL_analysis(int(test_info[1]))
         print("The level is {}".format(answer))
     if test_info[0] == "LDL":
         answer = LDL_analysis(int(test_info[1]))
         print("The level is {}".format(answer))
 
-def new_feature():
-    pass
-<<<<<<< HEAD
-=======
-def name_function():
-    first_name = input ("first name")
-    last_name = input ("last name")
->>>>>>> b3fb46d966dec6c978f732ba47b28e574591cc38
+
+# def new_feature():
+#    pass
+# <<<<<<< HEAD
+# =======
+# def name_function():
+#    first_name = input ("first name")
+#    last_name = input ("last name")
+# >>>>>>> b3fb46d966dec6c978f732ba47b28e574591cc38
+
 
 def interface():
     choice = 0
-    #while choice != 9:
+    # while choice != 9:
     while True:
         print("Cholesterol Calculator")
         print("Options: ")
@@ -52,6 +55,7 @@ def interface():
             return
         elif choice == "1":
             cholesterol_analysis()
-        
+
+
 if __name__ == "__main__":
     interface()
